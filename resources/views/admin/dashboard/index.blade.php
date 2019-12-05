3208873ce0401a3cfec5348ddb0e8e3185aedaa9
Nguyen Xuan Bach <bachnx2303@gmail.com>
2019-12-05 08:35:28 +0700

    fix: remove example html

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -29,7 +29,7 @@
             <!-- small box -->
             <div class="small-box bg-info">
               <div class="inner">
-                <h3>150</h3>
+                <h3></h3>
 
                 <p>Emails</p>
               </div>
@@ -44,7 +44,7 @@
             <!-- small box -->
             <div class="small-box bg-success">
               <div class="inner">
-                <h3>53<sup style="font-size: 20px">%</sup></h3>
+                <h3></h3>
 
                 <p>Groups</p>
               </div>
@@ -59,7 +59,7 @@
             <!-- small box -->
             <div class="small-box bg-warning">
               <div class="inner">
-                <h3>44</h3>
+                <h3></h3>
 
                 <p>Content Templates</p>
               </div>
