88ef0fdfc3f54272e0a58561612170adafb427db
Bach Nguyen <bachnx2303@gmail.com>
2020-12-15 16:25:36 +0700

    optimize: send email notify exceed quota.

--- app/Http/Middleware/RequestFilter.php
+++ app/Http/Middleware/RequestFilter.php
@@ -1,8 +1,10 @@
 <?php 
 namespace App\Http\Middleware;
 
+use App\Http\Controllers\EmailService;
 use Carbon\Carbon;
 use Closure;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 
 class RequestFilter 
@@ -21,6 +23,7 @@ class RequestFilter
         $message = "";
         $cacheKey =  'request::' . $clientSource . '::increment';
         $requestPerDay = $this->getRequestPerDayBySource($clientSource);
+        $this->emailNotify($clientSource, $cacheKey, $request);
         $isBlockRequest = false;
         $count = 1;
         if (Cache::has($cacheKey)) {
@@ -43,11 +46,37 @@ class RequestFilter
 
     private function getRequestPerDayBySource($source) {
         $retval = 0;
-        $configSources = config('request');
+        $configSources = config('request.source_quota');
         $retval = $configSources['default'];
         if ($source != '' && array_key_exists($source, $configSources)) {
             $retval = $configSources[$source];            
         }
         return $retval;
+    }
+
+    private function emailNotify($source, $cacheKey) {
+        $source = ($source !== '') ? $source : 'default';
+        $configSource = config('request.source_quota');
+        $notifyTo = config('request.notify_group');
+        $requestTimes = 0;
+        if (array_key_exists($source, $configSource)) {
+            $configQuota = $configSource[$source];
+        }
+        if (Cache::has($cacheKey)) {
+            $requestTimes = Cache::get($cacheKey);
+        }
+        if ($requestTimes == $configQuota) {
+            $email = new EmailService();
+            Cache::increment($cacheKey, 1);
+            $content = '<h4>Hi Admin, </h4>';
+            $content .= '<p>Request send email from source <strong>' . ucfirst($source) . '</strong>  reached their limit. Please check again!</p>';
+            $content .= '<p>Best Regard,</p>';
+            $request = new Request([
+                'to' => $notifyTo,
+                'subject' => 'Mailler[' . ucfirst($source) . ']: Daily quota exceeded.',
+                'content' => $content
+            ]);
+            $email->notifyEmail($request);
+        }
     }
 }
